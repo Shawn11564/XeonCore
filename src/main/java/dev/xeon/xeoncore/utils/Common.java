7f1099368f0ea62e63b1c5cd22760fd3bd587594
Dylan Perez <40929788+DylanPerez1@users.noreply.github.com>
2019-02-13 19:54:52 -0800

    Added two new methods, sendMinimalizedMsg and addMinimalizedLore.

--- src/main/java/dev/xeon/xeoncore/utils/Common.java
+++ src/main/java/dev/xeon/xeoncore/utils/Common.java
@@ -1,17 +1,20 @@
 package dev.xeon.xeoncore.utils;
 
-import dev.xeon.xeoncore.XeonCore;
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.command.CommandSender;
-
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import dev.xeon.xeoncore.XeonCore;
+
 public class Common {
-
+	
 	private static XeonCore instance = XeonCore.getInstance();
-
+	
 	public static void tell(CommandSender toWhom, String... messages) {
 		for (final String message : messages)
 			tell(toWhom, message);
@@ -25,6 +28,7 @@ public class Common {
 		return ChatColor.translateAlternateColorCodes('&', message);
 	}
 
+	@SuppressWarnings("deprecation")
 	public static boolean isValidPlayer(String player) {
 		if (Bukkit.getPlayerExact(player) != null)
 			return true;
@@ -61,5 +65,60 @@ public class Common {
 
 		return new Date(total);
 	}
+	
+	public static void sendMinimalizedMsg(String text, int limit, Player p) {
+		String message = "";
+		int check = 0;
+		while(check != text.length()) {
+			for(int i = 0; i < text.length(); i++) {
+				message += text.charAt(i);
+				check++;
+				if(message.length() >= limit) {
+					if(text.charAt(i) == ' ') {
+						p.sendMessage(message);
+						message = "";					
+					}
+					
+					if(check == text.length()) {
+						p.sendMessage(message);
+					}
+					
+				} else {
+					if(check == text.length()) {
+						p.sendMessage(message);
+					}
+				}
+			}	
+		}
+	}
 
+	public static void addMinimalizedLore(String text, ArrayList<String> al, int limit) {
+		String message = "";
+		int check = 0;
+		while(check != text.length()) {
+			for(int i = 0; i < text.length(); i++) {
+				message += text.charAt(i);
+				check++;
+				if(message.length() >= limit) {
+					if(text.charAt(i) == ' ') {
+						al.add(message);
+						al.add("------");
+						message = "";					
+					}
+					
+					if(check == text.length()) {
+						al.add(message);
+						al.add("------");
+					}
+					
+				} else {
+					if(check == text.length()) {
+						al.add(message);
+						al.add("------");
+					}
+				}
+			}	
+		}
+	}
+	
 }
